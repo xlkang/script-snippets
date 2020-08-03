9bbbbcff7b45c678775bb8af8979eb275b3ac3f4
xinlin kang <xlkang@MacBook-Pro.local>
2020-08-03 22:08:18 +0800

    .

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # script-snippets
 记录一些脚本片段
+
+
+## 运行
+
+```shell
+yarn            # 安装依赖
+yarn start      # 运行browser-sync启动http server
+```
+
+## 目录
+
+1. [演示v8闭包创建过程和render phase](https://github.com/xlkang/script-snippets/blob/master/main/closure.js)
+
--- closure.js
+++ closure.js
@@ -4,20 +4,40 @@
 
 var f = 1
 
+// 预解析，生成变量对象以及整条scope存放在堆内存中
 function wrap (p) {
     var a = 1;
     var b = p
 
     return function () {
-        console.log(b)
-    }
+        var c = 3
+        // v8预解析，将自由变量b由栈内存移入堆内存，
+        // 存放在变量对象上（猜测）
+        console.log('fn===', b)
+        function ccc () {
+            console.log('ccc===', c)
+        }
+        ccc()
+    }  
 }
 
+// first render phase
+// 创建fn1的[[Scope]]
+// 为 [[Scope]] 的变量对象节点取得新值
+// 取得闭包变量, 根据scope，查询变量值（捕获render phase），
+// 创建[[Closure]]，挂载到[[Scope]],
+// [[Scope]]挂载到fn1对象上
 var fn1 = wrap(1)
-
+console.dir(fn1)
 setTimeout(function(){}, 0)
 
 fn1()
+
+function fn3 () {}
+
+var fn4 = function () { console.log(fn3)}
+
+var n4 = fn4()
 
 setTimeout(function () {
     // another render phase
